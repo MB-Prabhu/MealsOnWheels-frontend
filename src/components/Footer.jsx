dbd9495ef74e034a4f380e0dd75fe07d9946b271
Prabhu kumar <prabhukumarmb@gmail.com>
2024-12-24 22:41:52 +0530

    updated about of meals on wheels in footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className='w-[90%] flex flex-col sm:flex-row gap-6 py-4 justify-center items-start sm:justify-between border-[#676767] border-b-2 '>
             <div className='sm:w-[30%]'>
              <img src={assets.logo} alt="" className='w-[60%]'/>
-             <p className='mt-2 leading-5'>our Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam at officia, officiis quibusdam perferendis facere impedit veniam corrupti aut ipsa ex dolore fugit fuga mollitia, cum ducimus natus temporibus pariatur.</p>
+             <p className='mt-2 leading-5'>Meal On Wheels is a online food ordering platform, which is providing great user experience and a way to order food online, including wide range of food varieties and offers and a lot more. Users have got great experience by using our product, and our team is working on to provide a more user friendly experience and new features  </p>
 
            <div className='flex gap-2 mt-4'>
               <a href="https://www.twitter.com" target='_blank' rel="noopener noreferrer"> <img src={assets.facebook_icon} alt="" className='w-10 h-10' /> </a>
